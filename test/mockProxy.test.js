ab4c16c498b0f5662bc4eca3d0c9db53af29c919
xuxinlai2002 <xxl@local.com>
2021-11-01 11:57:35 +0800

    add mock Proxy test

--- test/mockProxy.test.js
+++ test/mockProxy.test.js
@@ -13,11 +13,18 @@ describe(`Storage and upgradability example `, () => {
     const Box = await ethers.getContractFactory('Box');
     console.log('Deploying Box...');
     const box = await upgrades.deployProxy(Box, [42], { initializer: 'store' });
+
+
+    
     
     await box.deployed();
     console.log('Box deployed to:', box.address);
     let result = (await box.retrieve()).toString();
     console.log(result);
+
+    //console.log(upgrades);
+    const box1 = await upgrades.erc1967.getImplementationAddress(box.address);
+    console.log("real box1 address " + box1);
 
     //upgrade
     const BoxV2 = await ethers.getContractFactory('BoxV2');
@@ -25,12 +32,17 @@ describe(`Storage and upgradability example `, () => {
     await upgrades.upgradeProxy(box.address, BoxV2);
     console.log('Box upgraded');
 
-
     //work
     const boxV2 = await BoxV2.attach(box.address);
     await boxV2.increment();
     result = (await box.retrieve()).toString();
     console.log(result);
+
+    console.log("xxl boxV2 : " + boxV2.address);
+
+    const box2 = await upgrades.erc1967.getImplementationAddress(boxV2.address);
+    console.log("real box2 address " + box2);
+  
 
 
   })
